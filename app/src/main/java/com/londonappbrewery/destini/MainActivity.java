b8259e5ef15e890522197db21aeb53ea4ca6f6bc
hectorcedeno <hector.cedeno@icloud.com>
2019-02-14 23:10:06 -0500

    Project completed... I change some instructions Like ONClick I don't like the event on click I like more to add the event on the XML and created a function maybe becuase I'm more Swift and Objective C developer and is more familiar for me.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    int mStoryIndex = 1;
+    TextView mStoryTextView;
+    Button mButtonTop;
+    Button mButtonBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +22,54 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mButtonTop = findViewById(R.id.buttonTop);
+        mButtonBottom = findViewById(R.id.buttonBottom);
 
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
+        mStoryTextView.setText(R.string.T1_Story);
+        mButtonTop.setText(R.string.T1_Ans1);
+        mButtonBottom.setText(R.string.T1_Ans2);
 
     }
+
+    // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+
+    public void  mTopButton(View v) {
+       if (mStoryIndex == 1) {
+           mStoryIndex = 3;
+           mStoryTextView.setText(R.string.T3_Story);
+           mButtonTop.setText(R.string.T3_Ans1);
+           mButtonBottom.setText(R.string.T3_Ans2);
+       } else if (mStoryIndex == 3) {
+            mStoryIndex = 6;
+            mStoryTextView.setText(R.string.T6_End);
+       } else if (mStoryIndex == 2) {
+           mStoryIndex = 3;
+           mStoryTextView.setText(R.string.T3_Story);
+           mButtonTop.setText(R.string.T3_Ans1);
+           mButtonBottom.setText(R.string.T3_Ans2);
+       }
+
+        Toast myToast = Toast.makeText(this,"Storie # " + mStoryIndex, Toast.LENGTH_LONG);
+        myToast.show();
+    }
+    // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+    public void  mButtonBottom(View v) {
+        if (mStoryIndex == 1) {
+            mStoryIndex = 2;
+            mStoryTextView.setText(R.string.T2_Story);
+            mButtonTop.setText(R.string.T2_Ans1);
+            mButtonBottom.setText(R.string.T2_Ans2);
+        } else if (mStoryIndex == 3) {
+            mStoryIndex = 5;
+            mStoryTextView.setText(R.string.T5_End);
+        } else if (mStoryIndex == 2) {
+            mStoryIndex = 4;
+            mStoryTextView.setText(R.string.T4_End);
+        }
+
+        Toast myToast = Toast.makeText(this,"Storie # " + mStoryIndex, Toast.LENGTH_LONG);
+        myToast.show();
+    }
+
 }
